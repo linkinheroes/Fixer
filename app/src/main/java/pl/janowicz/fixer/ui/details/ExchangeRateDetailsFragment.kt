eda930e038fc9fe0941a8ffc92a95911d871b1db
Michał Janowicz <michal@aplikacjadrogowskaz.pl>
2019-07-21 20:45:06 +0200

    Exchange rate details view

--- app/src/main/java/pl/janowicz/fixer/ui/details/ExchangeRateDetailsFragment.kt
+++ app/src/main/java/pl/janowicz/fixer/ui/details/ExchangeRateDetailsFragment.kt
@@ -3,6 +3,7 @@ package pl.janowicz.fixer.ui.details
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.fragment_exchange_rate_details.*
 import pl.janowicz.fixer.R
@@ -17,6 +18,9 @@ class ExchangeRateDetailsFragment : Fragment(R.layout.fragment_exchange_rate_det
             exchange_rate_details_date_text_view.text = date
             exchange_rate_details_currency_name_text_view.text = currencyName
             exchange_rate_details_currency_rate_text_view.text = currencyRate
+        }
+        exchange_rate_details_back_image_view.setOnClickListener {
+            findNavController().popBackStack()
         }
     }
 }
